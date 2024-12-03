75a8e7ce517a0b1816312afee9ce811f8664c602
Ranjeth <ranjethravichandran@hotmail.com>
2024-12-03 01:56:02 +0000

    Hopefully this works, removal of Linting and Using Production address.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production'
+      ? '/interactive-gym'
+      : '',
+  },
+  output: "export",
 }
 
 export default nextConfig
