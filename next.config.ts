755a57694310ca2a9fd91dafb15c90a0ea584996
Ranjeth <ranjethravichandran@hotmail.com>
2024-12-03 02:03:43 +0000

    Im not sure what changed either.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   publicRuntimeConfig: {
     NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production'
       ? '/interactive-gym'
       : '',
   },
-  output: "export",
-}
+  output: 'export',
+};
 
-export default nextConfig
+export default nextConfig;
