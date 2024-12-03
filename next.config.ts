b78e45ac20435c9b04d10feaa04297cfa3e1a912
Ranjeth <ranjethravichandran@hotmail.com>
2024-12-03 01:01:00 +0000

    g

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SCULPT_GLB: process.env.NODE_ENV === 'production'
       ? '/interactive-gym/sculpt.glb' // production path
-      : '/sculpt.glb', // local development path
+      : '/interactive-gym/sculpt.glb', // local development path
   },
 };
 
